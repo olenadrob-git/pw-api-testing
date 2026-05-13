58379a59a30056c97aec533c869ba0c20f6cc391
Olena Drobysheva <el.kulinenkova@gmail.com>
2026-05-13 23:39:01 +0300

    54. Environment Variables
    npm install dotenv --save
    
    powerShell:
    $env:TEST_ENV="prod"; $env:PROD_USERNAME="blabla@test.com"; $env:PROD_PASSWORD="blablablabla"; npx playwright test smokeTest.spec.ts

--- api-test.config.ts
+++ api-test.config.ts
@@ -1,24 +1,33 @@
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+
 const processENV = process.env.TEST_ENV
 const env = processENV || 'dev'
-console.log('Test environment is: ' + env)
+console.log(`Test environment is: ${env}`)
 
 
 const config = {
     apiUrl: 'https://conduit-api.bondaracademy.com/api',
-    userEmail: 'olenatest@test.com',
-    userPassword: '!234rota'
+    userEmail: process.env.DEV_USERNAME as string,
+    userPassword: process.env.DEV_PASSWORD as string
 }
 
-if(env === 'qa'){
+if(env === 'qa') {
+    console.log(`config qa`)
     config.apiUrl = 'https://conduit-api.bondaracademy.com/api',
-    config.userEmail = 'olenatest2@test.com',
-    config.userPassword = '!234rota2'
+    config.userEmail =  process.env.QA_USERNAME as string,
+    config.userPassword = process.env.QA_PASSWORD as string
 }
+
 if(env === 'prod'){
+    // if(! process.env.PROD_USERNAME || ! process.env.PROD_PASSWORD){
+    //     throw Error('Missing required environment variable')
+    // }
     config.apiUrl = 'https://conduit-api.bondaracademy.com/api',
-    config.userEmail = 'olenatest@test.com',
-    config.userPassword = '!234rota'
-}
-
+    config.userEmail = process.env.PROD_USERNAME as string,
+    config.userPassword = process.env.PROD_PASSWORD as string
+    }
 
 export {config}

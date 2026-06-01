1dc30bf755b3504e179c90bdff807ca88e43e2ce
Olena Drobysheva <el.kulinenkova@gmail.com>
2026-06-01 12:49:23 +0300

    Modify API Response

--- tests/api-tests/workingWithAPI.spec.ts
+++ tests/api-tests/workingWithAPI.spec.ts
@@ -9,14 +9,29 @@ test.beforeEach('Run before each', async ({page} ) =>{
             body: JSON.stringify(tags)
         })
     })
+
+    await page.route('https://conduit-api.bondaracademy.com/api/articles?limit=10&offset=0', async route =>{
+        const response = await route.fetch()
+        const responseBody = await response.json()
+        responseBody.articles[0].title = "This is a test title"
+        responseBody.articles[0].description = "This is a test description"
+
+        await route.fulfill({
+            body: JSON.stringify(responseBody)
+        })
+    })
         
     await page.goto('https://conduit.bondaracademy.com/')
     console.log('This is executed before each test')
 })
+
 
 test('Has title and Mocking Tags', async ({ page }) => {
     await expect(page.locator('.navbar-brand')).toHaveText('conduit')
 
     await expect(page.locator('.tag-list')).toContainText('automation');
     await expect(page.locator('.tag-list')).toContainText('PLAYwRIGHT');
+
+    await expect(page.locator('app-article-preview h1').first()).toContainText('This is a test title');
+    await expect(page.locator('app-article-preview p').first()).toContainText('This is a test description');
 })
